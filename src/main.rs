3530f9fb4a979d8e6356062d470e9e5a55279506
Thomas Barron <t@tbarron.xyz>
2025-08-02 19:12:13 -0600

    making display more human readable

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,12 @@ fn redisCon() -> Result<redis::Connection, Error> {
 
 #[get("/")]
 async fn index() -> impl Responder {
-    HttpResponse::Ok().content_type("text/html").body(r#"<html>
+    HttpResponse::Ok().content_type("text/html").body(
+        r#"<html>
     <head>
+    <style>
+        #container { width: 600px }
+    </style>
       <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
         <script>
 const refreshAll = () => {
@@ -24,7 +28,11 @@ const refreshAll = () => {
     });
     fetch("/subtopics").then(res => {
         console.log("fetched subtopics");
-        res.text().then(t => document.getElementById("subtopics").textContent = t);
+        res.json().then(t => {
+            const technology = t.technology;
+            const values = Object.values(t).flatMap(x => x).map(x => x.name).join("<br/>");
+            document.getElementById("subtopics").innerHTML = `<h2>Topics:</h2><br/>${values}`;
+        });
     });
     setTimeout(refreshAll, 5000);
 };
@@ -35,22 +43,20 @@ window.onload = () => {
         </script>
     </head>
     <body>
-        <h1>Thomas Barron</h1>
-        <div id="currentSummary">
-        
-        </div><div id="subtopics"></div>
+        <div id="container">
+        <h1>Bluesky Monitor</h1>
+        <div id="currentSummary"></div>
+        <div id="subtopics"></div>
+        </div>
     </body>
-    </html>"#) // todo proper html response
+    </html>"#,
+    ) // todo proper html response
 }
 
 #[get("/summary")]
 async fn summary() -> Result<String> {
     let con = redisCon();
-
-
-    let result = con?
-        .get("currentSummary")
-        .or(Ok("".to_string()));
+    let result = con?.get("currentSummary").or(Ok("".to_string()));
 
     return result;
 }
@@ -58,19 +64,20 @@ async fn summary() -> Result<String> {
 #[get("/subtopics")]
 async fn subtopics() -> Result<String> {
     let con = redisCon();
-
-
-    let result2 = con?
-        .get("subtopics")
-        .or(Ok("".to_string()));
+    let result2 = con?.get("subtopics").or(Ok("".to_string()));
     return result2;
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Running httpserver");
-    HttpServer::new(|| App::new().service(index).service(summary).service(subtopics))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(|| {
+        App::new()
+            .service(index)
+            .service(summary)
+            .service(subtopics)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
