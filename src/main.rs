0c4f0b1934a6392a44393d148c3de3d8a0f044c8
Thomas Barron <t@tbarron.xyz>
2025-08-06 09:58:12 -0600

    adding and using img endpoint to populate cover photo; more styling; auto refresh checkboxes for stories and messages

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,4 @@
-use actix_web::{error, get, web, App, Error, HttpResponse, HttpServer, Responder, Result};
+use actix_web::{error, get, App, Error, HttpResponse, HttpServer, Responder, Result};
 // use redis::Client;
 use redis::Commands;
 
@@ -17,8 +17,13 @@ async fn index() -> impl Responder {
         r#"<html>
     <head>
     <style>
-        #container { width: 600px; float: left }
-        #right-container { width: 300px; float: right }
+        #container { width: 800px; float: left; display: inline-block; max-width: calc(100% - 300px); min-width: 350px; }
+        #right-container { width: 250px; float: right }
+        #coverPhoto { width: 350px; float: right; }
+        #coverStory { display: inline-block; }
+        .commentContainer { padding-top: 13px }
+        .halfcomment { display: inline-block; width: 50%; margin: 0; padding: 0; }
+        #messages { height: 600px; overflow: hidden; }
     </style>
       <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.0/css/all.min.css" integrity="sha512-DxV+EoADOkOygM4IR9yXP8Sb2qwgidEmeqAEmDKIOfPRQZOWbXCzLC6vjbZyy0vPisbH2SyW27+ddLVCN+OMzQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
@@ -79,37 +84,48 @@ const jetstream = new Jetstream();
 
 const last100 = [];
 jetstream.onCreate("app.bsky.feed.post", event => {
-    const t = event.detail.commit.record.text;
-    last100.unshift(t);
-    if (last100.length > 100) last100.pop();
-    document.getElementById("messages").textContent = last100.join("\n");
-    // console.log(t);
+    if (document.getElementById("messagesRefresh").checked) {
+        const t = event.detail.commit.record.text;
+        last100.unshift(t);
+        if (last100.length > 100) last100.pop();
+        document.getElementById("messages").textContent = last100.join("\n");
+    }
 });
 
 jetstream.start();
 
 const refreshAll = () => {
-    fetch("/news").then(res => {
-        res.json().then(j => {
-            // document.getElementById("news").innerHTML = JSON.stringify(j);
-            console.log(j);
-            document.getElementById("newspaper-title").textContent = j.newspaperName;
-            for (let i of [0,1,2,3,4]) {
-                let v = j.topics[i];
-                try {
-                    document.getElementById(`story${i+1}`).innerHTML = `<h3>${v.headline}</h3>${v.newsStoryFirstParagraph}<br/>${v.newsStorySecondParagraph}
-                    <div><i class="fa fa-user"></i>${v.gullibleComment}</di><div><i class="fa fa-user"></i>${v.skepticalComment}</div>`; 
-                } catch (e) { }
-            }
-            document.getElementById("coverStory").innerHTML = `<h2>${j.frontPageHeadline}</h2><p>${j.frontPageArticle}</p>`;
+    if (document.getElementById("refresh").checked) {
+        fetch("/news").then(res => {
+            res.json().then(j => {
+                // document.getElementById("news").innerHTML = JSON.stringify(j);
+                console.log(j);
+                document.getElementById("newspaper-title").textContent = j.newspaperName;
+                for (let i of [0,1,2,3,4]) {
+                    let v = j.topics[i];
+                    try {
+                        document.getElementById(`story${i+1}`).innerHTML = `<h3>${v.headline}</h3>${v.newsStoryFirstParagraph}<br/>${v.newsStorySecondParagraph}
+                        <div class="commentContainer"><div class="halfcomment"><i class="fa fa-user"></i>${v.gullibleComment}</div><div class="halfcomment"><i class="fa fa-user"></i>${v.skepticalComment}</div></div>`; 
+                    } catch (e) { }
+                }
+                document.getElementById("coverStory").innerHTML = `<img id="coverPhoto"/><h2>${j.frontPageHeadline}</h2><p>${j.frontPageArticle}</p>`;
+                document.getElementById("coverPhoto").src = document.imageUrl;
+
+                fetch("/img.jpg").then(res => {
+                    res.blob().then(blob => {
+                        const imageUrl = URL.createObjectURL(blob);
+                        document.imageUrl = imageUrl;
+                        document.getElementById("coverPhoto").src = imageUrl;
+                    });
+                });
+            });
         });
-    });
+    }
 
     setTimeout(refreshAll, 5000);
 };
 const refreshMessages = () => {
     fetch("/messages").then(res => {
-        // console.log("fetched messages");
         res.text().then(t => document.getElementById("messages").innerHTML = t);
     });
     //setTimeout(refreshMessages, 40); // disabling tweets auto update by default
@@ -117,7 +133,7 @@ const refreshMessages = () => {
 
 window.onload = () => {
     refreshAll();
-    refreshMessages();
+    // refreshMessages();
 };
         </script>
     </head>
@@ -126,8 +142,9 @@ window.onload = () => {
             <h1 id="newspaper-title">Bluesky Monitor</h1>
             <div id="coverStory"></div>
             <div id="story1"></div><div id="story2"></div><div id="story3"></div><div id="story4"></div><div id="story5"></div>
+            <i class="fa fa-refresh"></i><input checked type="checkbox" id="refresh"/>
         </div>
-        <div id="right-container"><h2>Messages:</h2><div id="messages"></div></div>
+        <div id="right-container"><div id="messages"></div><i class="fa fa-refresh"></i><input type="checkbox" checked id="messagesRefresh"/></div>
     </body>
     </html>"#,
     )
@@ -199,6 +216,17 @@ async fn news() -> Result<String> {
     return result;
 }
 
+#[get("/img.jpg")]
+async fn img() -> Result<HttpResponse> {
+    let con = redisCon();
+    let result = con?
+        .get("img")
+        .map(|x: Vec<u8>| HttpResponse::Ok().content_type("image/jpeg").body(x))
+        .or(Ok(HttpResponse::Ok().body("".to_string())));
+
+    return result;
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     println!("Running httpserver");
@@ -210,6 +238,7 @@ async fn main() -> std::io::Result<()> {
             // .service(messagesList)
             // .service(trends)
             .service(news)
+            .service(img)
     })
     .bind(("127.0.0.1", 8080))?
     .run()
